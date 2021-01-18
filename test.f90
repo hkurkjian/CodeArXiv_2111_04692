decd51db445b28340aaccbbd5c7ca3fdeffc6f42
SnnVL <sennevanloon@msn.com>
2021-01-18 14:23:30 +0100

    test low-q limit

--- test.f90
+++ test.f90
@@ -7,6 +7,7 @@ USE Zerom
 USE intpole
 USE intldc
 USE estM
+USE eqdetat
 IMPLICIT NONE
 
 REAL(QP) om,dom,M(1:2,1:2),dM(1:2,1:2),A(1:6),dMm(1:3),q
@@ -21,96 +22,124 @@ COMPLEX(QPC) Gamm(1:2,1:2),Matt(1:2,1:2),MatCat(1:2,1:2),det
 COMPLEX(QPC) SigPole(1:6)
 
 REAL(QP) ptq,ptom,ptM(1:3),ptdM(1:3)
-INTEGER iq, ik, iz
+INTEGER iq, ik, iz, ix
 REAL(QP) nnn,mmm,intell
 
 LOGICAL errtype1,errtype2,interpol
+REAL(QP) ThetaT, XxT, ccheckT
 
-EPSpp=1.0e-8_qp
-bla1=.TRUE.
-bla1=.FALSE.
-blaM=.TRUE.
-temperaturenulle=.TRUE.
-x0=1.0_qp
-x0crit=2.0_qp
-!
-x0=4.0_qp
+! ! x0=1.5_qp
+! fichier="specx0_1.5"
+! blaPole=.TRUE.
 
-q=3.74074074074074074074074074074074472E-002_qp
-om=2.0005_qp
+! ! do ik=1,50
+! !     do iz=1,50
+! !         k=ik/10.0_qp
+! !         zk=1.0_qp+iz/10.0_qp
+! !         A=selfEpole(k,zk)
+! !     enddo
+! ! enddo
+! k=1.1_qp
+! zk=2.5_qp
+! SigPole=selfEpole(k,zk)
+! write(6,*)"Sigma=",SigPole(:)
 
-q=0.112657197073092805303614801945519069_qp
-om=2.05085169619498758715291017720649464_qp
+open(10,file="testCIJ.dat")
+write(10,*)"x0, Theta, Xx, ccheck"
+do ix=1,101
+    x0=ix/10.0_qp-5.1_qp
+    ThetaT =4.0_qp*PI*I5(x0)
+    XxT    =4.0_qp*PI*I6(x0)
+    ccheckT=mc2sDelta(x0)
+    write(10,*)x0,ThetaT,XxT,ccheckT
+enddo
+close(10)
 
-xq=q
-call oangpp
+! EPSpp=1.0e-8_qp
+! bla1=.TRUE.
+! bla1=.FALSE.
+! blaM=.TRUE.
+! temperaturenulle=.TRUE.
+! x0=1.0_qp
+! x0crit=2.0_qp
+! !
+! x0=4.0_qp
 
-write(6,*)"q,om=",q,om
-write(6,*)"opp=",opp(1:3)
+! q=3.74074074074074074074074074074074472E-002_qp
+! om=2.0005_qp
 
-qpetit=0.05_qp*1/x0
-fich="BCSx04_nvo_augmente"
-call load_data(fich)
-est=interpolM_recerr(q,om)
-call unload_data
+! q=0.112657197073092805303614801945519069_qp
+! om=2.05085169619498758715291017720649464_qp
 
-call mat_pairfield(om,0.0_qp,det,Matt,Gamm)
-write(6,*)"om,xq,real(Matt(1,1))=",om,xq,real(Matt(1,1))
-write(6,*)"om,xq,real(Matt(2,2))=",om,xq,real(Matt(2,2))
-write(6,*)"om,xq,real(Matt(1,2))=",om,xq,real(Matt(1,2))
-write(6,*)"om,xq,real(Matt(1,1))=",om,xq,imag(Matt(1,1))
-write(6,*)"om,xq,real(Matt(2,2))=",om,xq,imag(Matt(2,2))
-write(6,*)"om,xq,real(Matt(1,2))=",om,xq,imag(Matt(1,2))
-write(6,*)
-write(6,*)"det=",det
-write(6,*)
+! xq=q
+! call oangpp
 
-call mat_pairfield_pttq(om,0.0_qp,det,Matt,Gamm)
-write(6,*)"om,xq,real(Matt(1,1))=",om,xq,real(Matt(1,1))
-write(6,*)"om,xq,real(Matt(2,2))=",om,xq,real(Matt(2,2))
-write(6,*)"om,xq,real(Matt(1,2))=",om,xq,real(Matt(1,2))
-write(6,*)"om,xq,real(Matt(1,1))=",om,xq,imag(Matt(1,1))
-write(6,*)"om,xq,real(Matt(2,2))=",om,xq,imag(Matt(2,2))
-write(6,*)"om,xq,real(Matt(1,2))=",om,xq,imag(Matt(1,2))
-write(6,*)
-write(6,*)"det=",det
-write(6,*)
+! write(6,*)"q,om=",q,om
+! write(6,*)"opp=",opp(1:3)
+
+! qpetit=0.05_qp*1/x0
+! fich="BCSx04_nvo_augmente"
+! call load_data(fich)
+! est=interpolM_recerr(q,om)
+! call unload_data
+
+! call mat_pairfield(om,0.0_qp,det,Matt,Gamm)
+! write(6,*)"om,xq,real(Matt(1,1))=",om,xq,real(Matt(1,1))
+! write(6,*)"om,xq,real(Matt(2,2))=",om,xq,real(Matt(2,2))
+! write(6,*)"om,xq,real(Matt(1,2))=",om,xq,real(Matt(1,2))
+! write(6,*)"om,xq,real(Matt(1,1))=",om,xq,imag(Matt(1,1))
+! write(6,*)"om,xq,real(Matt(2,2))=",om,xq,imag(Matt(2,2))
+! write(6,*)"om,xq,real(Matt(1,2))=",om,xq,imag(Matt(1,2))
+! write(6,*)
+! write(6,*)"det=",det
+! write(6,*)
+
+! call mat_pairfield_pttq(om,0.0_qp,det,Matt,Gamm)
+! write(6,*)"om,xq,real(Matt(1,1))=",om,xq,real(Matt(1,1))
+! write(6,*)"om,xq,real(Matt(2,2))=",om,xq,real(Matt(2,2))
+! write(6,*)"om,xq,real(Matt(1,2))=",om,xq,real(Matt(1,2))
+! write(6,*)"om,xq,real(Matt(1,1))=",om,xq,imag(Matt(1,1))
+! write(6,*)"om,xq,real(Matt(2,2))=",om,xq,imag(Matt(2,2))
+! write(6,*)"om,xq,real(Matt(1,2))=",om,xq,imag(Matt(1,2))
+! write(6,*)
+! write(6,*)"det=",det
+! write(6,*)
 
 
-fich="BCSx04_nvo_augmente"
-EPSpp=1.0e-8_qp
-bla1=.FALSE.
-blaM=.TRUE.
-temperaturenulle=.TRUE.
-x0=4.0_qp
+! fich="BCSx04_nvo_augmente"
+! EPSpp=1.0e-8_qp
+! bla1=.FALSE.
+! blaM=.TRUE.
+! temperaturenulle=.TRUE.
+! x0=4.0_qp
 
-fichom2 ="DONNEES/Tom1.dat"
-fichom2p="DONNEES/Tom1p.dat"
-k=2.1_qp
-zk=3.4_qp
-x0crit=0.0_qp
-x0=4.0_qp
-bla0=.TRUE.
-bla1=.TRUE.
-bla1=.FALSE.
-bla2=.TRUE.
-bla2=.FALSE.
-blaM=.TRUE.
-blaM=.FALSE.
-blaerr=.TRUE.
+! fichom2 ="DONNEES/Tom1.dat"
+! fichom2p="DONNEES/Tom1p.dat"
+! k=2.1_qp
+! zk=3.4_qp
+! x0crit=0.0_qp
+! x0=4.0_qp
+! bla0=.TRUE.
+! bla1=.TRUE.
+! bla1=.FALSE.
+! bla2=.TRUE.
+! bla2=.FALSE.
+! blaM=.TRUE.
+! blaM=.FALSE.
+! blaerr=.TRUE.
 
-EPSpp=1.0e-8_qp
-EPSom=1.0e-5_qp
-EPSq =1.0e-3_qp
-call bornesk
-!call lignesenergie(k)
-!le=(/l1,l2,l3,l4,l5,l6,l7,l8/)
-!call tri_q(le)
-!write(6,FMT="(A30,8G20.10)")"lignes d’énergie=",le
-!write(6,*)
-interpol=.TRUE.
-Mm=intim(k,zk,interpol,fich)
-stop
+! EPSpp=1.0e-8_qp
+! EPSom=1.0e-5_qp
+! EPSq =1.0e-3_qp
+! call bornesk
+! !call lignesenergie(k)
+! !le=(/l1,l2,l3,l4,l5,l6,l7,l8/)
+! !call tri_q(le)
+! !write(6,FMT="(A30,8G20.10)")"lignes d’énergie=",le
+! !write(6,*)
+! interpol=.TRUE.
+! Mm=intim(k,zk,interpol,fich)
+! stop
 
 !
 !k=(k11+k12)/2.
